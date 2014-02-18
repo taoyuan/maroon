30fad0350461881ae3e19f20cfdc730254bbf5d3
taoyuan <torworx@gmail.com>
2014-02-18 15:55:55 +0800

    0.0.2

--- test/support.js
+++ test/support.js
@@ -6,7 +6,7 @@ process.env.NODE_ENV = 'test';
 exports.t = chai.assert;
 
 exports.getMaroon = function() {
-    var maroon = require('../').create({});
+    var maroon = require('../').create();
     maroon.enable('quiet');
     return maroon;
 };
