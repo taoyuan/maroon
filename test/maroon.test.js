cf61e05332bbd346e0a417714b3c74529ffec94c
taoyuan <torworx@gmail.com>
2014-02-20 17:56:05 +0800

    refactor create function parameters to (app, options)

--- test/maroon.test.js
+++ test/maroon.test.js
@@ -5,7 +5,7 @@ var maroon = require('../')
 describe('maroon', function () {
     it('should forward configuration functions', function () {
         var app = {};
-        var m = maroon.create({ forward: true }, app);
+        var m = maroon.create(app, { forward: true });
         m.set('message', 'hello');
         t.equal('hello', app.get('message'));
     });
