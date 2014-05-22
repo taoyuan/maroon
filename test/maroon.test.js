42d76a4274840aea3d9bd2061816bc87d9f9b3c9
taoyuan <torworx@gmail.com>
2014-05-22 10:57:17 +0800

    remove argv support.

--- test/maroon.test.js
+++ test/maroon.test.js
@@ -6,7 +6,7 @@ describe('maroon', function () {
     it.only('should set right arguments with dot notation argv', function (done) {
         var m = s.getMaroon({argv: {a: {b: {c: 'hello'}}}});
         m.on('ready', function () {
-            t.deepEqual({b: {c: 'hello'}}, m.conf.settings.a);
+            t.deepEqual({b: {c: 'hello'}}, m.argv.a);
             done();
         });
     });
